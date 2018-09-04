24512215aacc201c9436ad1cf88857f71d61f8d7
Krzysztof Muhleisen <krzysztof.muhleisen@gmail.com>
2018-09-04 22:36:12 +0200

    stride_iterator added

---
+++ include/cpp_essentials/sq/detail/stride_iterator.hpp
@@ -0,0 +1,69 @@
+#ifndef CPP_ESSENTIALS_SQ_DETAIL_STRIDE_ITERATOR_HPP_
+#define CPP_ESSENTIALS_SQ_DETAIL_STRIDE_ITERATOR_HPP_
+
+#include <cpp_essentials/concepts/concepts.hpp>
+
+#include <cpp_essentials/core/iterator_facade.hpp>
+#include <cpp_essentials/core/iterator_range.hpp>
+
+namespace cpp_essentials::sq
+{
+
+namespace detail
+{
+
+template <class Iter>
+class stride_iterator
+    : public core::iterator_facade
+        < stride_iterator<Iter>
+        , std::input_iterator_tag
+        , concepts::iterator_reference<Iter>
+        , concepts::iterator_difference<Iter>>
+{
+public:
+    using base_type = core::iterator_facade
+        < stride_iterator<Iter>
+        , std::input_iterator_tag
+        , concepts::iterator_reference<Iter>
+        , concepts::iterator_difference<Iter>>;
+
+    INHERIT_ITERATOR_FACADE_TYPES(base_type)
+
+    stride_iterator(Iter iter, difference_type step, Iter last)
+        : _iter{ iter }
+        , _step{ step }
+        , _last{ last }
+    {
+    }
+
+    reference ref() const
+    {
+        return *_iter;
+    }
+
+    void inc()
+    {
+        auto n = _step;
+        while (_iter != _last && n > 0)
+        {
+            ++_iter;
+            --n;
+        }
+    }
+
+    bool is_equal(const stride_iterator& other) const
+    {
+        return _iter == other._iter;
+    }
+
+private:
+    Iter _iter;
+    difference_type _step;
+    Iter _last;
+};
+
+} /* namespace detail */
+
+} /* namespace cpp_essentials::sq */
+
+#endif /* CPP_ESSENTIALS_SQ_DETAIL_STRIDE_ITERATOR_HPP_ */
---
+++ include/cpp_essentials/sq/stride.hpp
@@ -0,0 +1,39 @@
+#ifndef CPP_ESSENTIALS_SQ_STRIDE_HPP_
+#define CPP_ESSENTIALS_SQ_STRIDE_HPP_
+
+#pragma once
+
+#include <cpp_essentials/core/adaptor.hpp>
+#include <cpp_essentials/core/iterator_range.hpp>
+#include <cpp_essentials/sq/detail/stride_iterator.hpp>
+
+namespace cpp_essentials::sq
+{
+
+namespace detail
+{
+
+struct stride_t : core::adaptable<stride_t>
+{
+    using adaptable::operator();
+
+    template
+        < class Range
+        , CONCEPT_IF(concepts::InputRange<Range>)>
+    auto operator ()(Range&& range, int step) const
+    {
+        auto b = std::begin(range);
+        auto e = std::end(range);
+        return core::make_range(
+            stride_iterator{ b, step, e },
+            stride_iterator{ e, step, e });
+    }
+};
+
+} /* namespace detail */
+
+static constexpr detail::stride_t stride = {};
+
+} /* namespace cpp_essentials::sq */
+
+#endif /* CPP_ESSENTIALS_SQ_STRIDE_HPP_ */

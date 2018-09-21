d2a2ec1a80e64b48137915312e20f26bb4c0ee19
Krzysztof Muhleisen <krzysztof.muhleisen@gmail.com>
2018-09-21 21:45:04 +0200

    Concepts renamed to PascalCase

--- include/cpp_essentials/cc/cc.hpp
+++ include/cpp_essentials/cc/cc.hpp
@@ -11,10 +11,25 @@ namespace cc
 {
 
 template <class T, class U>
-using is_same = std::enable_if_t<std::is_same_v<T, U>>;
+using Same = std::enable_if_t<std::is_same_v<T, U>>;
 
 template <class T, class U>
-using is_convertible = std::enable_if_t<std::is_convertible_v<T, U>>;
+using Convertible = std::enable_if_t<std::is_convertible_v<T, U>>;
+
+template <class T>
+using Arithmetic = std::enable_if_t<std::is_arithmetic_v<T>>;
+
+template <class T>
+using Integral = std::enable_if_t<std::is_integral_v<T>>;
+
+template <class T>
+using FloatingPoint = std::enable_if_t<std::is_floating_point_v<T>>;
+
+template <class T>
+using Enum = std::enable_if_t<std::is_enum_v<T>>;
+
+template <class T, class U>
+using BaseOf = std::enable_if_t<std::is_base_of_v<T, U>>;
 
 
 namespace detail
@@ -57,45 +72,48 @@ struct detector<void_t<Op<Args...>>, Op, Args...>
 
 
 template<template<class...> class Op, class... Args>
-using is_detected = typename detail::detector<void, Op, Args...>::value_t;
+using is = typename detail::detector<void, Op, Args...>::value_t;
 
 
 template <class T>
-using incrementable = decltype(++std::declval<T&>());
+using Incrementable = decltype(++std::declval<T&>());
 
 template <class T>
-using decrementable = decltype(--std::declval<T&>());
+using Decrementable = decltype(--std::declval<T&>());
 
 
 template <class T, class U>
-using addable = decltype(std::declval<T&>() + std::declval<U&>());
+using Addable = decltype(std::declval<T&>() + std::declval<U&>());
 
 template <class T, class U>
-using subtractable = decltype(std::declval<T&>() - std::declval<U&>());
+using Subtractable = decltype(std::declval<T&>() - std::declval<U&>());
 
 template <class T, class U>
-using multipliable = decltype(std::declval<T&>() * std::declval<U&>());
+using Multipliable = decltype(std::declval<T&>() * std::declval<U&>());
 
 template <class T, class U>
-using dividable = decltype(std::declval<T&>() / std::declval<U&>());
+using Dividable = decltype(std::declval<T&>() / std::declval<U&>());
 
 
 template <class T, class U = T>
-using equality_comparable = decltype(std::declval<T&>() == std::declval<U&>());
+using EqualityComparable = decltype(std::declval<T&>() == std::declval<U&>());
 
 template <class T, class U = T>
-using less_than_comparable = decltype(std::declval<T&>() < std::declval<U&>());
+using LessThanComparable = decltype(std::declval<T&>() < std::declval<U&>());
 
 template <class T, class U = T>
-using comparable = std::conjunction<equality_comparable<T, U>, less_than_comparable<T, U>>;
+using Comparable = std::conjunction<EqualityComparable<T, U>, LessThanComparable<T, U>>;
 
 
 template <class T>
-using has_ostream_operator = decltype(std::declval<std::ostream&>() << std::declval<const T&>());
+using HasOstreamOperator = decltype(std::declval<std::ostream&>() << std::declval<const T&>());
+
+template <class T>
+using HasIstreamIterator = decltype(std::declval<std::istream&>() >> std::declval<T&>());
 
 
 template <class T>
-using iterator = void_t
+using Iterator = void_t
     < typename std::iterator_traits<T>::value_type
     , typename std::iterator_traits<T>::reference
     , typename std::iterator_traits<T>::pointer
@@ -103,9 +121,9 @@ using iterator = void_t
     , typename std::iterator_traits<T>::difference_type>;
 
 template <class T>
-using range = void_t
-    < std::enable_if_t<is_detected<iterator, decltype(std::begin(std::declval<T&>()))>::value>
-    , std::enable_if_t<is_detected<iterator, decltype(std::end(std::declval<T&>()))>::value>>;
+using Range = void_t
+    < std::enable_if_t<is<Iterator, decltype(std::begin(std::declval<T&>()))>::value>
+    , std::enable_if_t<is<Iterator, decltype(std::end(std::declval<T&>()))>::value>>;
 
 namespace detail
 {
@@ -115,7 +133,7 @@ using range_iterator = decltype(std::begin(std::declval<T&>()));
 
 
 template <class Category, class T>
-using iterator_of_category = std::conjunction<iterator<T>, std::enable_if_t<std::is_base_of_v<Category, typename std::iterator_traits<T>::iterator_category>>>;
+using iterator_of_category = std::conjunction<Iterator<T>, std::enable_if_t<std::is_base_of_v<Category, typename std::iterator_traits<T>::iterator_category>>>;
 
 template <class Category, class T>
 using range_of_category = iterator_of_category<Category, range_iterator<T>>;
@@ -124,34 +142,50 @@ using range_of_category = iterator_of_category<Category, range_iterator<T>>;
 
 
 template <class T>
-using input_iterator = detail::iterator_of_category<std::input_iterator_tag, T>;
+using InputIterator = detail::iterator_of_category<std::input_iterator_tag, T>;
 
 template <class T>
-using forward_iterator = detail::iterator_of_category<std::forward_iterator_tag, T>;
+using ForwardIterator = detail::iterator_of_category<std::forward_iterator_tag, T>;
 
 template <class T>
-using bidirectional_iterator = detail::iterator_of_category<std::bidirectional_iterator_tag, T>;
+using BidirectionalIterator = detail::iterator_of_category<std::bidirectional_iterator_tag, T>;
 
 template <class T>
-using random_access_iterator = detail::iterator_of_category<std::random_access_iterator_tag, T>;
+using RandomAccessIterator = detail::iterator_of_category<std::random_access_iterator_tag, T>;
 
 template <class T>
-using output_iterator = void_t
-    < iterator<T>
+using OutputIterator = void_t
+    < Iterator<T>
     , decltype(*std::declval<T&>() = std::declval<detail::any_type&>())>;
 
 
 template <class T>
-using input_range = detail::range_of_category<std::input_iterator_tag, T>;
+using InputRange = detail::range_of_category<std::input_iterator_tag, T>;
 
 template <class T>
-using forward_range = detail::range_of_category<std::forward_iterator_tag, T>;
+using ForwardRange = detail::range_of_category<std::forward_iterator_tag, T>;
 
 template <class T>
-using bidirectional_range = detail::range_of_category<std::bidirectional_iterator_tag, T>;
+using BidirectionalRange = detail::range_of_category<std::bidirectional_iterator_tag, T>;
 
 template <class T>
-using random_access_range = detail::range_of_category<std::random_access_iterator_tag, T>;
+using RandomAccessRange = detail::range_of_category<std::random_access_iterator_tag, T>;
+
+
+template <class T>
+using NullaryFunction = decltype(std::declval<T&>()());
+
+template <class T, class A>
+using UnaryFunction = decltype(std::declval<T&>()(std::declval<A&>()));
+
+template <class T, class A, class B>
+using BinaryFunction = decltype(std::declval<T&>()(std::declval<A&>(), std::declval<B&>()));
+
+template <class T, class A>
+using UnaryPredicate = std::enable_if_t<std::is_convertible_v<UnaryFunction<T, A>, bool>>;
+
+template <class T, class A, class B>
+using BinaryPredicate = std::enable_if_t<std::is_convertible_v<BinaryFunction<T, A, B>, bool>>;
 
 
 template <class T>
@@ -160,21 +194,8 @@ using range_val = typename std::iterator_traits<detail::range_iterator<T>>::valu
 template <class T>
 using range_ref = typename std::iterator_traits<detail::range_iterator<T>>::reference;
 
-
 template <class T>
-using nullary_function = decltype(std::declval<T&>()());
-
-template <class T, class A>
-using unary_function = decltype(std::declval<T&>()(std::declval<A&>()));
-
-template <class T, class A, class B>
-using binary_function = decltype(std::declval<T&>()(std::declval<A&>(), std::declval<B&>()));
-
-template <class T, class A>
-using unary_predicate = std::enable_if_t<std::is_convertible_v<unary_function<T, A>, bool>>;
-
-template <class T, class A, class B>
-using binary_predicate = std::enable_if_t<std::is_convertible_v<binary_function<T, A, B>, bool>>;
+using range_cat = typename std::iterator_traits<detail::range_iterator<T>>::iterator_category;
 
 } /* namespace cc */
 

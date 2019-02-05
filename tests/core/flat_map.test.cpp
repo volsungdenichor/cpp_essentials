4d1142ef49fece1b1afa543b3aa59d3d1fd925f0
Krzysztof Muhleisen <krzysztof.muhleisen@gmail.com>
2019-02-05 21:06:58 +0100

    flat_map: UT added

---
+++ tests/core/flat_map.test.cpp
@@ -0,0 +1,13 @@
+#include <catch.hpp>
+#include <cpp_essentials/sq/sq.hpp>
+
+#include <../tests/test_helpers.hpp>
+
+using namespace cpp_essentials;
+
+TEST_CASE("flat_map")
+{
+    std::vector<std::string> vect = { "1", "2", "x", "4", "y" };
+
+    REQUIRE((vect | sq::flat_map(core::try_parse<int>)) == vec(1, 2, 4));
+}

bf620627f9d770596fdc5559cadc6429731680bc
Krzysztof Muhleisen <krzysztof.muhleisen@gmail.com>
2022-06-07 21:46:54 +0200

    Move all the stuff to "rubble", crucial functionality remains.

--- include/cpp_essentials/kine/animation.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cpp_essentials/kine/gradual.hpp>
-#include <cpp_essentials/kine/constant.hpp>
-#include <cpp_essentials/kine/repeat.hpp>
-#include <cpp_essentials/kine/reverse.hpp>
-#include <cpp_essentials/kine/sequence_phase.hpp>
-#include <cpp_essentials/kine/sequence.hpp>
